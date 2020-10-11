876d367dfc772df44db2f4a6bfe94eb0ae987024
Nicolas Olaechea <nicoolaechea11@gmail.com>
2020-10-11 18:34:06 -0300

    Agrega interfaz Beer y template del componente beer-list

--- src/app/beer-list/beer-list.component.ts
+++ src/app/beer-list/beer-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Beer } from './Beer';
 @Component({
   selector: 'app-beer-list',
   templateUrl: './beer-list.component.html',
@@ -7,13 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BeerListComponent implements OnInit {
 
-  beer = {
-    "name": "Negra juerte",
-    "type": "Porter",
-    "price": 100,
-    "stock": 5,
-    "image": "assets/img/porter.jpeg",
-  }
+  beers : Beer[] = [
+    {
+      "name": "Bitter Call Saul",
+      "type": "IPA",
+      "price": 180,
+      "stock": 5,
+      "img": "assets/img/porter.jpeg",
+      "clearance": false,
+    },
+    {
+      "name": "Red Red Wine",
+      "type": "Barley Wine",
+      "price": 200,
+      "stock": 3,
+      "img": "assets/img/porter.jpeg",
+      "clearance": true,
+    },
+    {
+      "name": "Golden Submarine",
+      "type": "Golden Ale",
+      "price": 180,
+      "stock": 0,
+      "img": "assets/img/porter.jpeg",
+      "clearance": false,
+    },
+    {
+      "name": "Golden Submarine 2",
+      "type": "Golden Ale",
+      "price": 230,
+      "stock": 0,
+      "img": "assets/img/porter.jpeg",
+      "clearance": false,
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
